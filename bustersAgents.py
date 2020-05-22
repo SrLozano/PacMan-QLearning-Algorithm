c72fa8ab027c411ced026db05e7190a6361ca6ec
SarayGrj <saraygrj@gmail.com>
2020-05-22 16:03:38 +0200

    arreglos

--- bustersAgents.py
+++ bustersAgents.py
@@ -1199,9 +1199,9 @@ class QLearningAgent(BustersAgent):
         self.actions = {"North":0, "East":1, "South":2, "West":3}
         self.table_file = open("qtable.txt", "r+")
         self.q_table = self.readQtable()
-        self.epsilon = 0  #Probabilidad de que se mueva random
-        self.alpha = 0 #Tasa de aprendizaje representa como de agresivo es el aprendizaje 
-        self.discount = 0 #Factor de descuento para dar mas importancia a las recompensas mas inmediatas
+        self.epsilon = 0.6  #Probabilidad de que se mueva random
+        self.alpha = 0 .2#Tasa de aprendizaje representa como de agresivo es el aprendizaje 
+        self.discount = 0.5 #Factor de descuento para dar mas importancia a las recompensas mas inmediatas
         self.past_state = None
 
 
